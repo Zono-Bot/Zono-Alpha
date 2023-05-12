89bdc995921ce8e7c5cca6ff68bab2739d3b42b4
adityaREDFLAG <121813330+adityaREDFLAG@users.noreply.github.com>
2023-05-12 18:35:40 +0000

    Added things

---
+++ Commands/roblox.js
@@ -0,0 +1,13 @@
+module.exports = ({
+name: "roblox",
+code: `$description [**Username:** $jsonRequest[https://api.roblox.com/users/get-by-username?username=$replaceText [ $message; ; ];UsernameNotFound; Username not found. ]
+**ID:** $jsonRequest[https://api.roblox.com/users/get-by-username?
+username=$replaceText[$message; ; ];IdNotFound; ID not found.]
+**Profile Link: ** https://www.roblox.com/users/$jsonRequest[https:// api.roblox.com/users/get-by-username?
+username=$replaceText [$message; ; ];IdNotFound; ID not found.]/profile]
+$color[BLUE]
+$argsCheck [>1; Please provide a username.]
+$onlyIf [$jsonRequest[https://api.roblox.com/users/get-by-username?username=$replaceText[$message; ; ];UserId; Error]!=Error; User not found. ]
+$onlyIf [ $message!=;Please provide a username.]
+$suppressErrors[An error occurred while executing the command.]`
+});
