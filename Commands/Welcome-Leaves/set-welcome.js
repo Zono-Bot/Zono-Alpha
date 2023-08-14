63ae7374828778150577e17a6e5eba9df1a49610
adityaREDFLAG <121813330+adityaREDFLAG@users.noreply.github.com>
2023-08-14 16:42:01 +0200

    fixed welcomes

--- Commands/Welcome-Leaves/set-welcome.js
+++ Commands/Welcome-Leaves/set-welcome.js
@@ -1,4 +1,4 @@
-module.exports = ({
+module.exports = {
     name:"set-welcome",
 
    code: `$title[New Welcome Channel]
@@ -12,6 +12,21 @@ $onlyif[$channelExists[$findChannel[$message[1];false]]==true; **$username** men
 $onlyIf[$hasPerms[$guildid;$authorid;managechannels]==true; **You are missing the \`managechannels\` permission**]
 $onlyIf[$hasPerms[$guildid;$clientid;managechannels]==true; **I am missing the \`managechannels\` permission**]
 `
-})
+},{
+    name: "remove-welcome",
+    code:`
+    $description[Welcome removed <#$mentionedChannels[1]>]
+
+
+    $resetguildVar[wchannel;$mentionedChannels[1]]
+
+    $onlyif[$channelExists[$findChannel[$message[1];false]]==true; **$username** mention a vaild channel]
+
+    $onlyIf[$hasPerms[$guildid;$authorid;managechannels]==true; **You are missing the \`managechannels\` permission**]
+    $onlyIf[$hasPerms[$guildid;$clientid;managechannels]==true; **I am missing the \`managechannels\` permission**]
+    `
+
+
+}
 
 
